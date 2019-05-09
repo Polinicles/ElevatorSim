6bbdd428e076692a1d0e1747c4025b302d548ff8
Sergi Besses <sergi.bema@gmail.com>
2019-05-09 19:17:58 +0200

    Fixed missing folders bug and added some commands to be executed

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -4,4 +4,16 @@ set -Eeuo pipefail
 
 /healthcheck-mysql.sh
 
+# Composer
+composer install --no-interaction
+composer clear-cache
+
+# Cache
+bin/console cache:clear
+bin/console cache:warmup
+
+# DB
+bin/console doctrine:schema:update --force
+bin/console doctrine:fixtures:load --append
+
 exec php-fpm -F
